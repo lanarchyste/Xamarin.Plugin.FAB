68208b75a1984cb87845b9bb74920c406fc9b29b
Julien Binet <julien.binet@agilitic.fr>
2018-03-19 09:56:19 +0100

    Correction du namespace

--- Library/FloatingActionButton.Droid/LinkerPleaseInclude.cs
+++ Library/FloatingActionButton.Droid/LinkerPleaseInclude.cs
@@ -1,15 +1,13 @@
-﻿using System;
-using Android.Runtime;
-using FAB.Droid;
+﻿using Android.Runtime;
 
-namespace FAB.iOS
+namespace FAB.Droid
 {
     [Preserve(AllMembers = true)]
     public class LinkerPleaseInclude
     {
         public void Include(FloatingActionButtonRenderer renderer)
         {
-            var r = new FloatingActionButtonRenderer();
+            var r = new FloatingActionButtonRenderer(null);
         }
     }
 }
